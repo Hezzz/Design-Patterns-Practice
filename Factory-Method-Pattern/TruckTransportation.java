5600c6ef3457f99416842c3e5eace25700fbe7ea
Hezzz <49769113+Hezzz@users.noreply.github.com>
2022-07-13 20:34:42 +0800

    Create TruckTransportation.java

---
+++ Factory-Method-Pattern/TruckTransportation.java
@@ -0,0 +1,7 @@
+public class TruckTransportation implements ITransportation{
+    @Override
+    public void deliver() {
+        System.out.println("Delivering through truck...");        
+    }
+    
+}
