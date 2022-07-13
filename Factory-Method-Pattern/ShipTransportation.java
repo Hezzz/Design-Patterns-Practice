9fd7dc2a269e21be347ffad0ba06ff45df34f772
Hezzz <49769113+Hezzz@users.noreply.github.com>
2022-07-13 20:34:58 +0800

    Create ShipTransportation.java

---
+++ Factory-Method-Pattern/ShipTransportation.java
@@ -0,0 +1,7 @@
+public class ShipTransportation implements ITransportation{
+    @Override
+    public void deliver() {
+        System.out.println("Delivering through ship...");        
+    }
+    
+}
